5fcd5b1fa74cba6ff1fb18e25eac432eacc17b58
nipunreddevil <nipunreddevil@gmail.com>
2014-01-18 15:09:56 +0530

    Initial commit

---
+++ examples/cli_disaggregate.py
@@ -0,0 +1,37 @@
+"""Illustrates disaggregation using NILMTK CO on the command line"""
+
+from ..smap_interface import SMAP
+from ..smap_interface import to_pd_series
+from nilmtk.building import Building
+from nilmtk.sensors.electricity import MainsName, Measurement
+from nilmtk.disaggregate.co_1d import CO_1d
+
+FLAT_NUMBER = 101
+UUID = '273e1b0f-ef25-5918-ad67-efa56a834d6e'
+START = 1387996200000
+END = 1388341800000
+DISAGG_FEATURE = Measurement('power', 'active')
+
+# Making SMAP connection
+smap = SMAP("http://192.168.1.40:9012/")
+
+# Getting data for UUID
+smap_readings = smap.get_readings(UUID, START, END)
+
+# Transform to NILMTK Mains
+df = to_pd_series(smap_readings)
+
+# Creating nilmtk building
+b = Building()
+
+# Attaching mains
+b.utility.electric.mains[MainsName(1, 1)] = df
+
+# Instantiating CO disaggregator
+disaggregator = CO_1d()
+
+# Loading model
+disaggregator.import_model(model.json)
+
+# Perform disaggregation
+disaggregator.disaggregate(b)
---
+++ smap_interface.py
@@ -0,0 +1,28 @@
+"""Contains methods to interact with sMAP interface to pull data according to
+sMAP query language"""
+
+import requests
+import pandas as pd
+from nilmtk.sensors.electricity import Measurement
+
+
+def to_pd_series(smap_data):
+    df = pd.DataFrame(
+        smap_data, columns=['timestamp', Measurement('power', 'active')])
+    df.index = pd.to_datetime(df.timestamp, unit='ms')
+    df = df.drop('timestamp', 1)
+    return df
+
+
+class SMAP(object):
+
+    def __init__(base_url, query_path="/api/query/", data_path="backend/api/data/"):
+        self.post_url = "".join([base_url, query_path])
+        self.data_path = "".join([base_url, data_path])
+
+    def get_readings(self, uuid, start_time, end_time):
+        query = "{}uuid/{}?starttime={}&endtime={}".format(self.data_path,
+                                                           uuid, start_time, end_time)
+        response = requests.get(query).json()
+        data = response[0]['Readings']
+        return data
